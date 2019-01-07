ce9cd49bcbca3bb25ede5302da40980b62618868
daniel <dennyparas28@gmail.com>
2019-01-07 17:04:59 +0800

    update bulma color var

--- src/App.vue
+++ src/App.vue
@@ -16,3 +16,33 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+  // Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+// Set your colors
+$primary: #008a94;
+$primary-invert: findColorInvert($primary);
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+    "white": ($white, $black),
+    "black": ($black, $white),
+    "light": ($light, $light-invert),
+    "dark": ($dark, $dark-invert),
+    "primary": ($primary, $primary-invert),
+    "info": ($info, $info-invert),
+    "success": ($success, $success-invert),
+    "warning": ($warning, $warning-invert),
+    "danger": ($danger, $danger-invert),
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
 
 Vue.use(Buefy)
 
