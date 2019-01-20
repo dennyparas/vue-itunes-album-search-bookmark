b3bf6a57512c9eb11191d16d5e54d36f452c9edd
dennyparas <dennyparas28@gmail.com>
2019-01-20 22:51:13 +0800

    add vue moment package

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import Buefy from 'buefy'
+import vueMoment from 'vue-moment'
 
 Vue.use(Buefy)
+Vue.use(vueMoment)
 
 Vue.config.productionTip = false
 
