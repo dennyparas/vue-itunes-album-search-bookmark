e7f72c45407ae34244a04dbefd69be5c327166c4
dennyparas <dennyparas28@gmail.com>
2019-02-02 20:27:11 +0800

    refactor code and add app error message

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import { Snackbar } from 'buefy/dist/components/snackbar'
 
 Vue.use(Vuex)
 
@@ -17,126 +18,139 @@ export default new Vuex.Store({
     isAlbumLoading: false,
     isAlbumTracksLoading: false,
     language: 'en_us',
-    pageType: 'search'
+    pageType: 'search',
+    appError: null
   },
   getters: {
-    SEARCH_QUERY (state) {
+    SEARCH_QUERY: (state) => {
       return state.settings.searchQuery
     },
-    INITIAL_SEARCH_QUERY (state) {
+    INITIAL_SEARCH_QUERY: (state) => {
       return state.settings.initialSearchQuery
     },
-    GET_ALBUMS (state) {
+    GET_ALBUMS: (state) => {
       return state.albums
     },
-    GET_ALBUM_TRACKS (state) {
+    GET_ALBUM_TRACKS: (state) => {
       return state.albumTracks
     },
-    GET_RECENT_SEARCH (state) {
+    GET_RECENT_SEARCH: (state) => {
       return state.recentSearch
     },
-    IS_ALBUM_LOADING (state) {
+    IS_ALBUM_LOADING: (state) => {
       return state.isAlbumLoading
     },
-    IS_ALBUM_TRACKS_LOADING (state) {
+    IS_ALBUM_TRACKS_LOADING: (state) => {
       return state.isAlbumTracksLoading
     },
-    SEARCH_FAILED (state) {
+    SEARCH_FAILED: (state) => {
       return state.searchFailed
     },
-    ALBUM_TRACKS_FAILED (state) {
+    ALBUM_TRACKS_FAILED: (state) => {
       return state.albumTracksFailed
     },
-    BOOKMARK_ALBUMS (state) {
+    BOOKMARK_ALBUMS: (state) => {
       return state.bookmarkAlbums.reverse()
     },
-    PAGE_TYPE (state) {
+    PAGE_TYPE: (state) => {
       return state.pageType
     },
-    SHOW_RECENT_SEARCH_BOX (state) {
+    SHOW_RECENT_SEARCH_BOX: (state) => {
       return state.showRecentSearchBox
     },
-    GET_SETTINGS (state) {
+    GET_SETTINGS: (state) => {
       return state.settings
     }
   },
   mutations: {
-    SET_SEARCH_QUERY (state, query) {
+    SET_SEARCH_QUERY: (state, query) => {
       state.pageType = 'search'
       state.settings.searchQuery = query
     },
-    SET_ALBUM (state, data) {
+    SET_ALBUM: (state, data) => {
       state.albums = data
     },
-    SET_ALBUM_TRACKS (state, data) {
+    SET_ALBUM_TRACKS: (state, data) => {
       state.albumTracks = data
     },
-    SEARCH_FAILED (state, action) {
+    SEARCH_FAILED: (state, action) => {
       state.searchFailed = action
     },
-    SET_RECENT_SEARCH (state, data) {
+    SET_RECENT_SEARCH: (state, data) => {
       state.pageType = 'search'
       state.recentSearch = data
     },
-    CLEAR_SEARCH (state) {
+    CLEAR_SEARCH: (state) => {
       state.albums = []
       state.settings.searchQuery = ''
     },
-    TOGGLE_RECENT_SEARCH (state) {
+    TOGGLE_RECENT_SEARCH: (state) => {
       state.showRecentSearchBox = !state.showRecentSearchBox
     },
-    SET_BOOKMARK_ALBUMS (state, albums) {
+    SET_BOOKMARK_ALBUMS: (state, albums) => {
       state.bookmarkAlbums = albums
     },
-    IS_ALBUM_LOADING (state, action) {
+    IS_ALBUM_LOADING: (state, action) => {
       state.isAlbumLoading = action
     },
-    IS_ALBUM_TRACKS_LOADING (state, action) {
+    IS_ALBUM_TRACKS_LOADING: (state, action) => {
       state.isAlbumTracksLoading = action
     },
-    SET_PAGE_TYPE (state, type) {
+    SET_PAGE_TYPE: (state, type) => {
       if (type === 'bookmarks') { state.settings.searchQuery = '' }
       state.pageType = type
     },
-    SET_SETTINGS (state, settings) {
+    SET_SETTINGS: (state, settings) => {
       state.settings = settings
     },
-    SET_ALBUM_TRACKS_FAILED (state, action) {
+    SET_ALBUM_TRACKS_FAILED: (state, action) => {
       state.albumTracksFailed = action
       state.albumTracks = []
     },
-    RESET_ALBUM_TRACKS (state) {
+    RESET_ALBUM_TRACKS: (state) => {
       state.albumTracks = []
+    },
+    APP_ERROR: (state, message) => {
+      state.appError = message
+      Snackbar.open({
+        message: message,
+        type: 'is-danger',
+        position: 'is-top',
+        actionText: 'Reload App',
+        indefinite: true,
+        onAction: () => {
+          window.location.reload()
+        }
+      })
     }
   },
   actions: {
-    SEARCH_ALBUMS ({ commit, dispatch }, payload) {
-      // show loading animation
-      commit('IS_ALBUM_LOADING', true)
-      return axios.get(`${payload.url}`)
-        .then((response) => {
-          if (response.data.results.length === 0) {
-            // if search response data results is empty commit search failed and clear the search input
-            commit('CLEAR_SEARCH')
-            commit('SEARCH_FAILED', true)
-            commit('IS_ALBUM_LOADING', false)
-          } else {
-            // assign the search data results to set album state and query to set search query state
-            commit('IS_ALBUM_LOADING', false)
-            commit('SEARCH_FAILED', false)
-            commit('SET_ALBUM', response.data.results)
-            commit('SET_SEARCH_QUERY', payload.query)
-            dispatch('SAVE_TO_RECENT_SEARCH', payload.query)
-          }
-        })
-        .catch(() => {
-          // if error commit search failed and clear the search input
+    SEARCH_ALBUMS: async ({ commit, dispatch }, payload) => {
+      try {
+        // show loading animation
+        commit('IS_ALBUM_LOADING', true)
+        const { data } = await axios.get(`${payload.url}`)
+        if (data.results.length === 0) {
+          // if search response data results is empty commit search failed and clear the search input
           commit('CLEAR_SEARCH')
           commit('SEARCH_FAILED', true)
           commit('IS_ALBUM_LOADING', false)
-        })
+        } else {
+          // assign the search data results to set album state and query to set search query state
+          commit('IS_ALBUM_LOADING', false)
+          commit('SEARCH_FAILED', false)
+          commit('SET_ALBUM', data.results)
+          commit('SET_SEARCH_QUERY', payload.query)
+          dispatch('SAVE_TO_RECENT_SEARCH', payload.query)
+        }
+      } catch (err) {
+        // if error commit search failed and clear the search input
+        commit('CLEAR_SEARCH')
+        commit('IS_ALBUM_LOADING', false)
+        commit('APP_ERROR', err.message)
+      }
     },
-    SAVE_TO_RECENT_SEARCH ({ commit }, payload) {
+    SAVE_TO_RECENT_SEARCH: ({ commit }, payload) => {
       try {
         let recentSearch = []
         // check if localstorage have recent search datas
@@ -154,11 +168,11 @@ export default new Vuex.Store({
         }
         // assign a new array to the set recent search state
         commit('SET_RECENT_SEARCH', recentSearch)
-      } catch (e) {
-        alert(e.message)
+      } catch (err) {
+        commit('APP_ERROR', err.message)
       }
     },
-    GET_RECENT_SEARCH ({ commit }) {
+    GET_RECENT_SEARCH: ({ commit }) => {
       try {
         // assign recent_search localstorage dato to a recentSearch variable
         const recentSearch = localStorage.getItem('recent_search')
@@ -167,11 +181,11 @@ export default new Vuex.Store({
           // if not null assign a new array to the  recentSearch state
           commit('SET_RECENT_SEARCH', JSON.parse(recentSearch))
         }
-      } catch (e) {
-        alert(e.message)
+      } catch (err) {
+        commit('APP_ERROR', err.message)
       }
     },
-    REMOVE_RECENT_SEARCH_ITEM ({ commit }, item) {
+    REMOVE_RECENT_SEARCH_ITEM: ({ commit }, item) => {
       try {
         // assign recent_search localstorage to recentSearchItems variable
         const recentSearchItems = JSON.parse(localStorage.getItem('recent_search'))
@@ -187,11 +201,11 @@ export default new Vuex.Store({
         }
         // assign the new recentSearchItems array to the the set recent search state
         commit('SET_RECENT_SEARCH', recentSearchItems)
-      } catch (e) {
-        alert(e.message)
+      } catch (err) {
+        commit('APP_ERROR', err.message)
       }
     },
-    BOOKMARK_ALBUM ({ commit }, payload) {
+    BOOKMARK_ALBUM: ({ commit }, payload) => {
       try {
         // destructure and assign payload album objects to the new variables
         const { artistName, collectionCensoredName, artworkUrl100, primaryGenreName, collectionViewUrl, collectionId } = payload.album
@@ -227,11 +241,11 @@ export default new Vuex.Store({
           }
         }
         commit('SET_BOOKMARK_ALBUMS', bookmarkAlbums)
-      } catch (e) {
-        alert(e.message)
+      } catch (err) {
+        commit('APP_ERROR', err.message)
       }
     },
-    GET_BOOKMARK_ALBUMS ({ commit }) {
+    GET_BOOKMARK_ALBUMS: ({ commit }) => {
       try {
         // assign bookmark_albums localstorage to bookmarkAlbums variable
         const bookmarkAlbums = localStorage.getItem('bookmark_albums')
@@ -239,11 +253,11 @@ export default new Vuex.Store({
           // if not null assign the new bookmark albums array to the bookmark albums state
           commit('SET_BOOKMARK_ALBUMS', JSON.parse(bookmarkAlbums))
         }
-      } catch (e) {
-        alert(e.message)
+      } catch (err) {
+        commit('APP_ERROR', err.message)
       }
     },
-    GET_SETTINGS ({ commit, state }) {
+    GET_SETTINGS: ({ commit, state }) => {
       try {
         // assign settings localstorage to settings variable
         const settings = localStorage.getItem('settings')
@@ -255,42 +269,43 @@ export default new Vuex.Store({
           // if null set the default state settings to localstorage
           localStorage.setItem('settings', JSON.stringify(state.settings))
         }
-      } catch (e) {
-        alert(e.message)
+      } catch (err) {
+        commit('APP_ERROR', err.message)
       }
     },
-    UPDATE_SETTINGS ({ commit, state }, payload) {
+    UPDATE_SETTINGS: ({ commit, state }, payload) => {
+      try {
       // destructure and assign payload objects and state to the new variables
-      const { settingValue, settingName } = payload
-      const { settings } = state
-      // update settings by reassigning the settings object name to the payload object name
-      settings[settingName] = settingValue
-      // assign the new settings array to the settings state
-      commit('SET_SETTINGS', settings)
-      // assign the new settings array to the settings localstorage
-      localStorage.setItem('settings', JSON.stringify(settings))
+        const { settingValue, settingName } = payload
+        const { settings } = state
+        // update settings by reassigning the settings object name to the payload object name
+        settings[settingName] = settingValue
+        // assign the new settings array to the settings state
+        commit('SET_SETTINGS', settings)
+        // assign the new settings array to the settings localstorage
+        localStorage.setItem('settings', JSON.stringify(settings))
+      } catch (err) {
+        commit('APP_ERROR', err.message)
+      }
     },
-    GET_ALBUM_TRACKS ({ commit, state }, payload) {
-      // reset album tracks
-      if (state.albumTracks.length > 0) {
-        commit('RESET_ALBUM_TRACKS')
-      } else {
+    GET_ALBUM_TRACKS: async ({ commit, state }, payload) => {
+      try {
         // show loading animation
         commit('IS_ALBUM_TRACKS_LOADING', true)
-        return axios.get(`${payload.url}`)
-          .then((response) => {
-            if (response.data.results.length === 0) {
-              commit('SET_ALBUM_TRACKS_FAILED', true)
-              commit('IS_ALBUM_TRACKS_LOADING', false)
-            } else {
-              commit('SET_ALBUM_TRACKS', response.data.results)
-              commit('IS_ALBUM_TRACKS_LOADING', false)
-            }
-          })
-          .catch(() => {
-            commit('SET_ALBUM_TRACKS_FAILED', true)
-            commit('IS_ALBUM_TRACKS_LOADING', false)
-          })
+        // reset album tracks
+        if (state.albumTracks.length > 0) {
+          commit('RESET_ALBUM_TRACKS')
+        }
+        const { data } = await axios.get(`${payload.url}`)
+        if (data.results.length === 0) {
+          commit('SET_ALBUM_TRACKS_FAILED', true)
+          commit('IS_ALBUM_TRACKS_LOADING', false)
+        } else {
+          commit('SET_ALBUM_TRACKS', data.results)
+          commit('IS_ALBUM_TRACKS_LOADING', false)
+        }
+      } catch (err) {
+        commit('APP_ERROR', err.message)
       }
     }
   }
