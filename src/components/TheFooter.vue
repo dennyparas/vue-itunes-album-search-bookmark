037c7e0497fd31ce3276628f7edf2c3fd6b20d76
dennyparas <dennyparas28@gmail.com>
2019-02-04 16:53:03 +0800

    update footer css style and class

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,13 +2,13 @@
   <footer class="footer">
     <div class="container">
     <div class="columns is-multiline is-mobile">
-      <div class="column is-6">
-        <p>
+      <div class="column is-6-desktop is-6-tablet is-6-widescreen is-12-mobile ">
+        <p class="has-text-centered-mobile">
        Made with <a href="https://vuejs.org/" target="_blank"><i class="fas fa-heart"></i> Vue.js</a>
       </p>
       </div>
-      <div class="column is-6">
-        <p class="has-text-right">
+      <div class="column is-6-desktop is-6-tablet is-6-widescreen is-12-mobile">
+        <p class="has-text-right-desktop has-text-right-widescreen  has-text-right-tablet has-text-centered-mobile">
        Powered by: <a href="https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/#legal" target="_blank"> Free iTunes Search API </a>
       </p>
       </div>
